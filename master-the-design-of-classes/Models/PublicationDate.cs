608673323426675d9f8901499f650902352b731e
kristjan kangro <kristjan.kangro@visma.com>
2024-08-26 22:58:48 +0300

    PublicationDate.cs

--- master-the-design-of-classes/Models/PublicationDate.cs
+++ master-the-design-of-classes/Models/PublicationDate.cs
@@ -2,6 +2,6 @@ namespace Demo.Models;
 
 public abstract record PublicationDate;
 
-public sealed record FullDate(DateOnly Date);
-public sealed record YearMonth(int Year, int Month);
+public sealed record FullDate(DateOnly Date) : PublicationDate;
+public sealed record YearMonth(int Year, int Month) : PublicationDate;
 public sealed record Year(int Number) : PublicationDate;
