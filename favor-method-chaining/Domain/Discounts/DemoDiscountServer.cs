bf1143302775c7c8044606ccd35683506c91bb40
kristjan kangro <kristjan.kangro@visma.com>
2024-09-07 11:20:39 +0300

    nested calls ()

--- favor-method-chaining/Domain/Discounts/DemoDiscountServer.cs
+++ favor-method-chaining/Domain/Discounts/DemoDiscountServer.cs
@@ -10,5 +10,26 @@ public class DemoDiscountServer : IDiscountServer
     public DemoDiscountServer(Person author) =>
         _martinFowler = author;
 
-    public IDiscount GetDiscounts() => new NoDiscount();
+    //public IDiscount GetDiscounts() => new NoDiscount();
+    
+    // business request #1
+    // 30% discount on martin fowler patterns books
+    // or then 20% on books of patterns
+    // or flat 10% all books RelativeDiscount(0.1M)
+    
+    //nested calls todod fix to chain
+    public IDiscount GetDiscounts() =>
+        FirstOf(
+            OnBooksBy(_martinFowler, OnTopic("patterns",RelativeDiscount(.3M))),
+            OnTopic("patterns", RelativeDiscount(.2M)),
+            OnAllBooks(RelativeDiscount(.1M)));
+
+    public IDiscount OnTopic(string topic, IDiscount other) => new TitleContentDiscount(topic, other);
+    public IDiscount OnBooksBy(Person author, IDiscount other) => new BookAuthorDiscount(author, other);
+    private IDiscount RelativeDiscount(decimal fact) => new RelativeDiscount(fact);
+    private IDiscount OnAllBooks(IDiscount other) => new AllBooksDiscount(other);
+    
+    private IDiscount FirstOf(params IDiscount[] discounts)
+        => discounts.Reverse().Aggregate((alt, primary) => new DefaultIfEmptyDiscount(primary, alt));
+
 }
