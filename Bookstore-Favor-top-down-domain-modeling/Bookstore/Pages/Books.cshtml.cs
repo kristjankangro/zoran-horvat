c0192dfa087a48ad4069b263f31b29d8e6452c08
kristjan kangro <kristjan.kangro@visma.com>
2024-10-06 21:14:12 +0300

    frontend done

--- Bookstore-Favor-top-down-domain-modeling/Bookstore/Pages/Books.cshtml.cs
+++ Bookstore-Favor-top-down-domain-modeling/Bookstore/Pages/Books.cshtml.cs
@@ -12,20 +12,30 @@ public class BooksModel : PageModel
     private readonly ILogger<BooksModel> _logger;
     private readonly BookstoreContext _context;
 
-    public IEnumerable<BookHeader> Books { get; set; } =
-        Array.Empty<BookHeader>();
-    
-    public BooksModel(ILogger<BooksModel> logger, BookstoreContext context)
+    public IEnumerable<(string AuthorName, string Title)> Books { get; set; } = Array.Empty<(string, string)>();
+
+    /// <summary>
+    /// Just for mocking data as first step
+    /// </summary>
+    public IEnumerable<(string AuthorName, string Title)> LoremIpsumBooks =>
+        new[]
+        {
+            ("Thomas Mann", "Doctor Faustus"),
+            ("Aristotle", "Rhetoric"),
+            (string.Empty, "1001 nights")
+        };
+
+    public BooksModel(ILogger<BooksModel> logger)
     {
         _logger = logger;
-        _context = context;
     }
 
     public void OnGet()
     {
-        this.Books = this._context.Books
-            .Include(book => book.Author)
-            .ToList()
-            .ToBookHeaders();
+        this.Books = LoremIpsumBooks.ToList();
+        // this.Books = this._context.Books
+        //     .Include(book => book.Author)
+        //     .ToList()
+        //     .ToBookHeaders();
     }
 }
